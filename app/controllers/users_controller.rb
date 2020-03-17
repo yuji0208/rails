84af957b36f0eed9a1c9192b5d867fdccfef81a5
EC2 Default User <ec2-user@ip-172-31-39-54.ap-northeast-1.compute.internal>
2020-03-17 08:27:30 +0000

    更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,14 @@ class UsersController < ApplicationController
   end
   
   def create
+    binding.pry
+    @user = User.new(name: params[:user][:name], email: params[:user][:email]) #params=送られてきた値を受け取るメソッド
+    if @user.save
+      redirect_to root_path , success: "登録が完了しました"
+      #redirect_toは指定したpath(URL)に転送
+    else #@user.saveが失敗した場合
+      flash.now[:danger] = "登録に失敗しました" #.nowをつけた場合、移動した次のページのみメッセージが表示されるようになり、その次のページでは表示されない仕組みになります。
+      render :new #データを取得して表示する場合
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    validates :name, presence: true#validate=検証
+    validates :email, presence: true
 end
