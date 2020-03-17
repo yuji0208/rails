fda7247221539604c560f5661bb5ce6a4811ff34
EC2 Default User <ec2-user@ip-172-31-39-54.ap-northeast-1.compute.internal>
2020-03-17 13:20:09 +0000

    進捗

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
     validates :name, presence: true#validate=検証
+    validates :name, length: { maximum: 15}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true
+    validates :email, format: { with: VALID_EMAIL_REGEX}
+    VALID_PASSWORD_REGEX = /\A(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)\z/
+    validates :password, length: { in: 8..32 } 
+    validates :password, presence: true
+    validates :password, format: { with: VALID_PASSWORD_REGEX}
     
     has_secure_password
 end
